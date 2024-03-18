86ebb875a7b349488961591a3ad045f5d0497e3e
mickova2 <mickova2@spsejecna.cz>
2024-03-18 10:59:21 +0100

    victory

--- src/Game.java
+++ src/Game.java
@@ -42,17 +42,21 @@ public class Game {
         System.out.println("player " + player);
         System.out.println("choose your characters placement");
         answer = sc.nextInt();
-        field[answer-1] = player;
+        if (!field[answer-1].equals("X") || !field[answer-1].equals("O")){
+            field[answer-1] = player;
+        }
+        for (int i = 0; i < field.length; i++) {
+            System.out.print(field[i]);
+        }
+
     }
 
     public void start(){
        System.out.println(intro());
-       while (!victory()){
+       while (!victory("X", "O")){
            try {
-               for (int i = 0; i < 5; i++) {
-                   choose("X");
-                   choose("O");
-               }
+               choose("X");
+               choose("O");
            }catch (IndexOutOfBoundsException e){
 
            }
@@ -60,15 +64,30 @@ public class Game {
 
     }
 
-    public boolean victory(){
-
+    public boolean victory(String player1, String player2){
+        boolean vic = false;
+        String line = "";
         for (int i = 0; i < 8; i++) {
             switch (i){
-                case 0:
+                case 0 -> line = field[0] + field[1] + field[2];
+                case 1 -> line = field[3] + field[4] + field[5];
+                case 2 -> line = field[6] + field[7] + field[8];
+                case 3 -> line = field[1] + field[3] + field[6];
+                case 4 -> line = field[1] + field[4] + field[7];
+                case 5 -> line = field[2] + field[5] + field[8];
+                case 6 -> line = field[0] + field[4] + field[8];
+                case 7 -> line = field[2] + field[4] + field[6];
+            }
+            if (line.equals(player1+player1+player1)){
+                System.out.println("player " + player1 + " won");
+                vic = true;
+            } if (line.equals(player2+player2+player2)){
+                System.out.println("player " + player2 + " won");
+                vic = true;
             }
         }
 
-        return false;
+        return vic;
     }
 
     @Override
