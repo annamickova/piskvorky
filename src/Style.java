db75127a8d73ef8474f25789c6f6f41a7b0685fd
LittleLoading <dhourda@gmail.com>
2024-03-18 10:02:42 +0100

    basic style of the Cross

---
+++ src/Style.java
@@ -0,0 +1,18 @@
+public class Style {
+
+
+    public String display(int number, Game player) {
+
+        System.out.print("" + '\n' +
+                "| = = = = = = = |" + '\n' +
+                "| 1--2--3|" + '\n' +
+                "| 4--5--6|" + '\n' +
+                "| 7--8--9|" + '\n' +
+                "| = = = = = = = |");
+
+        String player1 = "X";
+        String player2 = "O";
+
+
+    }
+}
