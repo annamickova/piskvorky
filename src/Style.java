cf8061cd6c490ab533b97c90cfc74bf89e4b8213
LittleLoading <dhourda@gmail.com>
2024-03-18 10:57:39 +0100

    make it look beter

--- src/Style.java
+++ src/Style.java
@@ -1,12 +1,12 @@
 public class Style {
 
 
-        public String displayBoard(int number, String[] field, String player) {
-            field[number] = player;
-        return "------------------------" +'\n' +
-                "| " + field[0] + " | " + field[1] + " | " + field[2] + " | " +'\n'+
+    public String displayBoard(int number, String[] field, String player) {
+        field[number] = player;
+        return "------------------------" + '\n' +
+                "| " + field[0] + " | " + field[1] + " | " + field[2] + " | " + '\n' +
                 "| " + field[3] + " | " + field[4] + " | " + field[5] + " | " + '\n' +
-                "| " + field[6] + " | " + field[7] + " | " + field[8] + " | " + '\n'+ "------------------------";
+                "| " + field[6] + " | " + field[7] + " | " + field[8] + " | " + '\n' + "------------------------";
 
 
     }
